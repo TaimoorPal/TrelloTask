be71341c0bae98d6d71f2118bfac910fed1e9462
TaimoorPal <67574214+TaimoorPal@users.noreply.github.com>
2022-12-12 03:12:46 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,54 +1,55 @@
 package TrelloTask;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Main {
 	WebDriver driver;
 	POM obj = new POM(driver);
-	@SuppressWarnings("static-access")
-	@BeforeMethod
+	@BeforeTest
 	public void Setup() {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\chromedriver.exe");
+				"C:\\Users\\4397\\Documents\\SQA\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://trello.com/");
-		//PropertyConfigurator.configure("log4j.properties");
+		PropertyConfigurator.configure("log4j.properties");
 		
 	}
 	
-	@Test
+	@Test(priority=1)
 	public void Login() throws InterruptedException {
-		obj.Login();	
+		obj.Login();
 	}
-	@Test
+	@Test(priority=2)
 	public void CreateBoard() throws InterruptedException {
 		obj.CreateBoard();
 	}
-	@Test
+	@Test(priority=3)
 	public void MakeLists() throws InterruptedException {
 		obj.CreateFirstList();
 		obj.CreateSecondListList();
 		
 	}
-	@Test
+	@Test(priority=4)
 	public void AddCard() throws InterruptedException {
 		obj.AddCard();
 		
 	}
-	@Test
+	@Test(priority=5)
 	public void MoveCard() throws InterruptedException {
 		obj.MoveCard();
 		
 	}
-	@Test
+	@Test(priority=6)
 	public void EditCard() throws InterruptedException {
 		obj.editcardname();
 		
 	}
-	@Test
+	@Test(priority=7)
 	public void DeleteCard() throws InterruptedException {
 		obj.DeleteCard();
 		
@@ -56,3 +57,4 @@ public class Main {
 	
 	
 }
+
